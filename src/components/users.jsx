b564ad5e8e3183060e8fc41b9eb0c5356dc95fee
maksym-vovk <mvx.vovk@gmail.com>
2021-12-13 14:47:22 +0200

    Test spaces

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -30,8 +30,8 @@ const Users = () => {
                 <table className="table">
                     <thead>
                     <tr>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Качества</th>
+                        <th scope="col">Имя </th>
+                        <th scope="col">Качества </th>
                         <th scope="col">Профессия</th>
                         <th scope="col">Встретился, раз</th>
                         <th scope="col">Оценка</th>
