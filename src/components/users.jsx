975cd052dad63cd19b05d051f7c682e88e48f0e1
maksym-vovk <mvx.vovk@gmail.com>
2021-12-13 15:02:43 +0200

    Update spaces

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -31,7 +31,7 @@ const Users = () => {
                     <thead>
                     <tr>
                         <th scope="col">Имя </th>
-                        <th scope="col">Качества </th>
+                        <th scope="col">Качества</th>
                         <th scope="col">Профессия</th>
                         <th scope="col">Встретился, раз</th>
                         <th scope="col">Оценка</th>
