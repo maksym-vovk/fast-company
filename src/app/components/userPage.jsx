608ef55ce52a4877826485720e86e5148d29b781
maksym-vovk <mvx.vovk@gmail.com>
2022-01-09 18:45:36 +0200

    Add routing to the app

---
+++ src/app/components/userPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import api from "../api";
+import QualitiesList from "./qualitiesList";
+import { useHistory } from "react-router-dom";
+
+const UserPage = ({ userId }) => {
+    const history = useHistory();
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        api.users.getById(userId).then(data => setUser(data));
+    });
+
+    const handleClick = () => {
+        history.push("/users");
+    };
+
+    if (user) {
+        return (
+            <div>
+                <h1>{user.name}</h1>
+                <h2>Profession: {user.profession.name}</h2>
+                <QualitiesList qualities={user.qualities}/>
+                <p>Completed meetings: {user.completedMeetings}</p>
+                <p>Rate: {user.rate}</p>
+                <button onClick={handleClick}>All users</button>
+            </div>
+        );
+    } else {
+        return <h1>Loading...</h1>;
+    }
+};
+
+UserPage.propTypes = {
+    userId: PropTypes.string.isRequired
+};
+
+export default UserPage;
---
+++ src/app/layouts/users.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import UsersList from "../components/usersList";
+import UserPage from "../components/userPage";
+
+const Users = () => {
+    const params = useParams();
+    const { userId } = params;
+
+    return (
+        <>
+            {userId ? <UserPage userId={userId}/> : <UsersList/>}
+        </>
+    );
+};
+
+export default Users;
